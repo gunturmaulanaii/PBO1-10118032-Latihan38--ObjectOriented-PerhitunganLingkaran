ebdf02d3525c09f53226a63924835cb42167065c
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-10-16 22:24:59 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan38-(ObjectOriented)PerhitunganLingkaran/src/pbo1/pkg10118032/latihan38/objectoriented/perhitunganlingkaran/PBO110118032Latihan38ObjectOrientedPerhitunganLingkaran.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118032.latihan38.objectoriented.perhitunganlingkaran;
+
+/**
+ *
+ * @author Guntur Maulana I
+ */
+public class PBO110118032Latihan38ObjectOrientedPerhitunganLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Lingkaran ling = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        ling.validasiInput();
+        ling.hasilPerhitungan(ling.diameter);
+    }
+    
+}
